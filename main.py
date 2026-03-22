6e0d5fd2a101377275855ab742cb27cf634e6736
yuliyaBarashka <zohn12333@gmail.com>
2026-03-22 20:37:18 +0700

    main

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from src.models import Category, Product
 
 if __name__ == '__main__':
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0,5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    product1 = Product("Samsung Galaxy S23 Ultra",
+                       "256GB, Серый цвет, 200MP камера",
+                       180000.0,
+                       5)
+    product2 = Product("Iphone 15",
+                       "512GB, Gray space",
+                       210000.0,
+                       8)
+    product3 = Product("Xiaomi Redmi Note 11",
+                       "1024GB, Синий",
+                       31000.0,
+                       14)
 
     print(product1.name)
     print(product1.description)
